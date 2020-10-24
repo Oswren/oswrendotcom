a14199e1700c0e15e103b7a95ad11d5e09d30209
Toby Brown <toby.brown@microlise.com>
2020-10-24 21:46:17 +0100

    added first four birds and functionality to copy them (currently not hooked up to buttons yet)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpBirdsComponent } from './components/http-birds/http-birds.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/http-birds/http-birds.component.ts
+++ src/app/components/http-birds/http-birds.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-http-birds',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HttpBirdsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private copyCompleteSnackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  copyLinkToBirb(birbToCopy: string): void {
+    const dummyElement = document.createElement('input');
+    const textToCopy = window.location.href + '/' + birbToCopy + '.png';
+    // add temporary element to dom
+    document.body.appendChild(dummyElement);
+    dummyElement.value = textToCopy;
+    // copy contents of temporary element
+    dummyElement.select();
+    document.execCommand('copy');
+    // delete the temprary element from dom
+    document.body.removeChild(dummyElement);
+
+    this.openCopyCompleteSnackBar('Link Copied!');
+  }
+
+  openCopyCompleteSnackBar(snackBarText: string): void {
+    this.copyCompleteSnackBar.open(snackBarText, 'Okay', {
+      duration: 1000
+    });
+  }
 }
