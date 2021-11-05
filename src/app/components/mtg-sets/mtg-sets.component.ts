1300860295cc0a05e347c28369e4532c3879cdee
Toby Brown <toby.br314@gmail.com>
2021-11-05 01:53:58 +0000

    functional (locally) mtg sets page

--- src/app/components/mtg-sets/mtg-sets.component.ts
+++ src/app/components/mtg-sets/mtg-sets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { OswrenApiService } from 'src/app/services/oswren-api.service';
 
 @Component({
@@ -7,15 +7,27 @@ import { OswrenApiService } from 'src/app/services/oswren-api.service';
   styleUrls: ['./mtg-sets.component.scss']
 })
 export class MtgSetsComponent implements OnInit {
-
-  public setData: any;
+  public chosenSetCode: string = "";
+  public numberOfCardsInSet: number;
+  @Input() setData: Array<Array<any>> = [];
 
   constructor(private oswrenApiService: OswrenApiService) { }
 
   ngOnInit(): void {
   }
 
-  getAllCardsForSet(setCode: string) {
-    this.setData = this.oswrenApiService.getCardSet(setCode);
+  getAllCardsForSet(): void {
+    this.oswrenApiService.getCardSet(this.chosenSetCode).subscribe((res: Array<any>) => {
+      this.setData = [];
+      this.numberOfCardsInSet = res.length;
+      while (res.length) {
+        this.setData.push(res.splice(0, 25));
+      }
+    });
+  }
+
+  isValidSetCode(): boolean {
+    
+    return !!this.chosenSetCode && this.chosenSetCode.length === 3;
   }
 }
--- src/app/services/oswren-api.service.ts
+++ src/app/services/oswren-api.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from  '@angular/common/http';
+import { takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OswrenApiService {
-
-  private REST_API_URL = "https://localhost:44328/MTG";
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+  private REST_API_URL = "https://localhost:44328/MTG/Cards/";
   
   constructor(private httpClient: HttpClient) { }
 
-  public getCardSet(setCode: string): any {
-    return this.httpClient.get(this.REST_API_URL + setCode);
+  public getCardSet(setCode: string): Observable<any> {
+    return this.httpClient.get(this.REST_API_URL + setCode).pipe(takeUntil(this.destroy$));
   }
 }
